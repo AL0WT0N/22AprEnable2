58ce67ad74482863748bfdc3df8ef9fffb4b04f8
ALowton <ALowton@QA.com>
2022-05-20 13:22:05 +0100

    AL: Added calc starter

--- JS/DOM/inputs.js
+++ JS/DOM/inputs.js
@@ -1,20 +1,33 @@
 'use strict';
 
 // Selectors
+// Inputs
 let inputOne = document.querySelector("#inputOne");
-let buttonOne = document.querySelector("#buttonOne");
+let inputTwo = document.querySelector("#inputTwo");
+
+// Buttons
+let addBtn = document.querySelector("#addBtn");
+let clearBtn = document.querySelector("#clearBtn");
+
+// Divs
 let mainDiv = document.querySelector("#mainDiv");
 
 // Functionality
-let myFunc = () => {
-    let inputOneValue = inputOne.value;
+let add = () => {
+    let valueOne = parseInt(inputOne.value);
+    let valueTwo = parseInt(inputTwo.value);
 
     let valueDiv = document.createElement("div");
-    let myValue = document.createTextNode(inputOneValue);
+    let myValue = document.createTextNode(`${valueOne} + ${valueTwo} = ${valueOne + valueTwo}`);
 
     valueDiv.appendChild(myValue);
     mainDiv.appendChild(valueDiv);
 }
 
+let clearResults = () => {
+    mainDiv.innerHTML = "";
+}
+
 // Event Listeners
-buttonOne.addEventListener("click", myFunc);
+addBtn.addEventListener("click", add);
+clearBtn.addEventListener("click", clearResults);
